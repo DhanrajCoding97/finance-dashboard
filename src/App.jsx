badf232106bd897939e44a21a436ff684b35c0dc
dhanraj shetty <dhanrajcoding@gmail.com>
2026-04-03 18:49:04 +0530

    initalized react vite with tailwind css & shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import { Button } from "./components/ui/button"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-   <h1 className='text-2xl text-yellow-400'>Finance Tracker</h1>
+   <h1 className='text-2xl text-yellow-400'>Finance Tracker </h1>
+   <Button variant="outline" size="lg">Hello</Button>
    </>
   )
 }
